82624366273c4cc7bf364d1f17e5be410ff061e6
Murali Elavarthi <muralis2p@gmail.com>
2025-06-12 10:38:35 +0530

    m1

--- vpc-module-test/vpc.tf
+++ vpc-module-test/vpc.tf
@@ -1,5 +1,10 @@
 module "vpc" {
     #source = "../Terraform-aws-vpc"
+
+    # referring git terraform module - we have to reconfigure after updating this
+    # >terraform init -reconfigure
+    # After doing this entire module code will come and sit inside the project folder
+    # here, i have kept both module and project in same folder
     source = "git::https://github.com/DAWS-82S/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
@@ -10,4 +15,7 @@ module "vpc" {
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = true
+
+    # if some updates happened in the module, if you want that updates to your project
+    # > terrafrom get-update - this will be download latest module code to your project
 }
